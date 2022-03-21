5274d5036465cb4d834af10ab095e28c2d8d25fb
Ascedete <patrickschneider2017@outlook.de>
2022-03-21 16:06:20 +0100

    added external requirement FileData to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="ParserCombinators", version='0.1', description="Parser Combinators", 
-packages=find_packages("parsers", exclude="test"))
+setup(
+    name="ParserCombinators",
+    version="0.1",
+    description="Parser Combinators",
+    packages=find_packages("parsers", exclude="test"),
+    install_requires=["FileData"],
+)
