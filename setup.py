9acb7520c9521aad90f05c03e4d3a42164b570d3
pts <pts@melexis.com>
2022-03-25 10:32:23 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version="0.1",
     description="Parser Combinators",
     packages=find_packages("parsers", exclude="test"),
-    install_requires=["FileData @ git+https://github.com/Ascedete/FileData.git"],
+    install_requires=["FileData @ git+https://github.com/Ascedete/FileData.git@master"],
+    url="https://github.com/Ascedete/Parsers",
 )
