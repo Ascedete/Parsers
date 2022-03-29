cebdf1210af70e5e5aa7209e2c0601f8e07d5d20
pts <pts@melexis.com>
2022-03-29 15:35:22 +0200

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ParserCombinators",
-    version="0.1.2",
+    version="0.1.3",
     description="Parser Combinators",
     packages=["parsers"],
     install_requires=["FileData @ git+https://github.com/Ascedete/FileData.git@master"],
