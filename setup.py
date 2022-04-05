165a3f0055945c7b66d2d666f0f733014bd9851b
pts <1567-pts@users.noreply.gitlab.melexis.com>
2022-04-05 15:16:14 +0200

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ParserCombinators",
-    version="0.1.8",
+    version="0.1.9",
     description="Parser Combinators",
     packages=["parsers"],
     install_requires=[
